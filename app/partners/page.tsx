0e9c7d0ffad3f94d230608b6fa825466ec00234d
Caleb <211212128+calebephrem@users.noreply.github.com>
2026-09-19 12:54:10 +0300

    feat: add utm source and other analytic parameters to partner links

--- app/partners/page.tsx
+++ app/partners/page.tsx
@@ -43,7 +43,7 @@ export default function Partners() {
               <h1 className="text-2xl">{partner.name}</h1>
               <span className="text-text-secondary">{partner.description}</span>
               <Link
-                href={partner.url}
+                href={`${partner.url}?utm_source=devhub&utm_medium=partner&ref=devhub`}
                 target="_blank"
                 rel="noopener noreferrer"
               >
